2b7577f210f8d0ceb4a2433135d61e06c342d908
Jaideep Singh <jaisidhu2004@gmail.com>
2023-12-29 17:59:26 +0530

    29/12/23

--- db.js
+++ db.js
@@ -12,14 +12,11 @@
 const mongoose = require('mongoose');
 
 // Use environment variable or default to a local MongoDB connection
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/CodingArena';
+// const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/CodingArena';
 
 const ConnectToMongo = async () => {
     try {
-        await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("Mongoose connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+
+require('dotenv').config()
 const express = require("express");
 const app = express();
-const port = 3001;
+
+const port = process.env.PORT || 3001;
+
 const ConnectToMongo = require("./db");
 const User = require("./User");
 const bcrypt = require("bcryptjs");
